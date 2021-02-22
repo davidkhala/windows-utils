2b540ff323f7e1b7927047534a9fe8f8c35e9434
David Liu <david-khala@hotmail.com>
2021-02-22 16:49:25 +0800

    Update powershell.sh

--- powershell.sh
+++ powershell.sh
@@ -2,7 +2,8 @@
 ## TODO Test
 
 setup(){
-    # 改为远程签名 执行策略，以允许执行.ps脚本
+    # 改为RemoteSigned 执行策略，以允许执行.ps脚本
+    # 否则在Restricted策略情况下，不允许执行脚本
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 }
 download(){
