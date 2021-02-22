d1d710c72e12c0e59d71c8b0e4439ddd8f57df8a
David Liu <david-khala@hotmail.com>
2021-02-22 16:48:01 +0800

    Update powershell.sh

--- powershell.sh
+++ powershell.sh
@@ -1,5 +1,10 @@
 # unix-like shell compatible in PowerShell
 ## TODO Test
+
+setup(){
+    # 改为远程签名 执行策略，以允许执行.ps脚本
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+}
 download(){
   # `wget` is an alias for `Invoke-WebRequest`
   wget $1 -outfile $2
