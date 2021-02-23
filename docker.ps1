573cdc9f3d3767395136625c3ec20d4383ec0a98
David Liu <david-khala@hotmail.com>
2021-02-23 17:20:56 +0800

    RestartNeeded

--- docker.ps1
+++ docker.ps1
@@ -1,9 +1,7 @@
 
 function Enable-HyperV {
-  Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All
-  
-}
-function Install-HyperV {
   Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
-  
+
+  Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All
+  # RestartNeeded
 }
