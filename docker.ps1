d6739a869bfa5d03502b1f574c274176e70d7111
David Liu <david-khala@hotmail.com>
2021-02-23 20:51:40 +0800

    Install-WindowsFeature prompt with error

--- docker.ps1
+++ docker.ps1
@@ -1,6 +1,5 @@
 
 function Enable-HyperV {
-  Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 
   Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All
   # RestartNeeded
