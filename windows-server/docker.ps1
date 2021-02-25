032b1251dabeb7dfc5fd4d8d6eb81f88b8ddedec
David Liu <david.yx.liu@oracle.com>
2021-02-25 16:29:50 +0000

    backup

--- windows-server/docker.ps1
+++ windows-server/docker.ps1
@@ -7,18 +7,15 @@ function Install-Docker {
         
         
     )
+    Install-Module DockerProvider
+    Install-Package Docker -ProviderName DockerProvider
     if ($Kernal = ='Linux') {
-        Install-Module DockerProvider
-        Install-Package Docker -ProviderName DockerProvider # -RequiredVersion preview
-        [Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "1", "Machine")
+        # -RequiredVersion preview
+        Remove-Service -Name Docker ## TODO powershell 7 only
+        New-Service -Name Docker-Linux -BinaryPathName "C:\Program Files\Docker\dockerd.exe --run-service --experimental"
 
     }
-    else {
-        Install-Module -Name DockerMsftProvider -Repository PSGallery -Force # If you're prompted to install the NuGet provider, type `Y` to install it as well.
-        Install-Package -Name docker -ProviderName DockerMsftProvider
-        [Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "$null", "Machine")
-        
-    }
+    
     
 }
 function Uninstall {
@@ -27,11 +24,7 @@ function Uninstall {
         [Parameter(Position = 0)]
         [string]$Kernal = 'NT'
     )
-    if ($Kernal = ='Linux') {
-        Uninstall-Package Docker -ProviderName DockerProvider
-    }
-    else {
-        Uninstall-Package -Name docker -ProviderName DockerMSFTProvider
-
-    }
+    
+    Uninstall-Package Docker -ProviderName DockerProvider
+    
 }
