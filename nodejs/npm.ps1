02f2d3df0fe9cdc684f1af01916bb9a5b4a44d11
David Liu <david.yx.liu@oracle.com>
2021-04-03 09:58:21 +0800

    Add npm-cache

--- nodejs/npm.ps1
+++ nodejs/npm.ps1
@@ -2,6 +2,8 @@ function Clear-Global-Modules {
     try {
         Remove-Item -Recurse -Force  -ErrorAction Ignore "$env:LOCALAPPDATA\node-gyp"
         Remove-Item -Recurse -Force  -ErrorAction Ignore "$env:APPDATA\npm"
+        Remove-Item -Recurse -Force  -ErrorAction Ignore "$env:APPDATA\npm-cache"
+        
     }
     catch {
         $_
