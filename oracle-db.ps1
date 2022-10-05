df8a9ef5614e3f36286c6307112f5516d894df91
David Liu <david.yx.liu@oracle.com>
2022-10-05 23:13:15 +0800

    Update oracle-db.ps1

--- oracle-db.ps1
+++ oracle-db.ps1
@@ -2,5 +2,7 @@
 function Install-LightClient {
     
     Invoke-WebRequest -Uri https://download.oracle.com/otn_software/nt/instantclient/instantclient-basiclite-windows.zip -OutFile instantclient-basiclite-windows.zip -UseBasicParsing
+    Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vc_redist.x64.exe -OutFile vc_redist17.x64.exe -UseBasicParsing
+    
 #     TODO how to unzip and install 
 }
