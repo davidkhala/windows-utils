28a5da3a3ba80fb7d9afcff5843957e4aef59bc6
David Liu <david-khala@hotmail.com>
2023-04-11 21:43:58 +0800

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -26,7 +26,7 @@ function Remove-RF {
 function Use-RemoteSigned {
     # 改为RemoteSigned 执行策略，以允许执行PowerShell脚本
     # 否则在Restricted策略情况下，不允许执行脚本
-    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
     
 }
 function Invoke-RemoteScript{
