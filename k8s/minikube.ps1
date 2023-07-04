677a1b450c97c1d5428a16dea1efd1a75252c794
David Liu <david-khala@hotmail.com>
2023-07-04 23:59:26 +0800

    Update minikube.ps1

--- k8s/minikube.ps1
+++ k8s/minikube.ps1
@@ -2,5 +2,6 @@ function Choco-Install {
   choco install minikube -y
 }
 function Docker-Start {
+  Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Tools-All -All
   minikube start --container-runtime=docker --vm=true
 }
