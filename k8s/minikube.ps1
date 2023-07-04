5305e6de7078eced916bfca245d95bc7e5f7a7af
David Liu <david-khala@hotmail.com>
2023-07-05 00:16:48 +0800

    Update minikube.ps1

--- k8s/minikube.ps1
+++ k8s/minikube.ps1
@@ -4,6 +4,7 @@ function Choco-Install {
 function Docker-Start {
   
   minikube start --container-runtime=docker --vm=true
+  # If minikube fails to start, see the [drivers page](https://minikube.sigs.k8s.io/docs/drivers/) for help setting up a compatible container or virtual-machine manager.
 }
 function Docker-Alias{
   & minikube -p <profile> docker-env --shell powershell | Invoke-Expression
