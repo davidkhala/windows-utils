41b558cc0f73fd51b63a77062b5df0741c76ab81
David Liu <david-khala@hotmail.com>
2023-07-04 23:52:13 +0800

    Update moby.ps1

--- docker/moby.ps1
+++ docker/moby.ps1
@@ -1,6 +1,6 @@
 function Choco-Install{
   choco install Containers Microsoft-Hyper-V --source windowsfeatures
-  choco install docker-engine
+  choco install docker-engine -y
   # This package creates the group `docker-users` and adds the installing user to it.
   #   In order to communicate with docker you will need to log out and back in.
 
