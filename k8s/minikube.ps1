c729afdebc4d20c0fda9f08b9c37212b34608502
David Liu <david-khala@hotmail.com>
2023-07-05 00:13:37 +0800

    Update minikube.ps1

--- k8s/minikube.ps1
+++ k8s/minikube.ps1
@@ -5,3 +5,6 @@ function Docker-Start {
   
   minikube start --container-runtime=docker --vm=true
 }
+function Docker-Alias{
+  & minikube -p <profile> docker-env --shell powershell | Invoke-Expression
+}
