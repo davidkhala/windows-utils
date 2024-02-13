e3ccc8776d9b8ce72615053985fa57ac00f2f805
David Liu <david-khala@hotmail.com>
2024-02-13 21:08:48 +0800

    Create agent.ps1

---
+++ aws/cloudwatch/agent.ps1
@@ -0,0 +1,24 @@
+# TODO make it runnable
+Install(){
+msiexec /i https://amazoncloudwatch-agent.s3.amazonaws.com/windows/amd64/latest/amazon-cloudwatch-agent.msi
+
+
+}
+Configure(){
+pip install toml-cli
+# workaround for cloudagent known issue
+toml add_section --toml-path C:\ProgramData\Amazon\AmazonCloudWatchAgent\common-config.toml credentials
+toml set --toml-path C:\ProgramData\Amazon\AmazonCloudWatchAgent\common-config.toml credentials.shared_credential_profile default
+toml set --toml-path C:\ProgramData\Amazon\AmazonCloudWatchAgent\common-config.toml credentials.shared_credential_file $env:USERPROFILE\.aws\credentials
+
+  # The output config.json is stored in current directory
+& "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-config-wizard"
+
+
+}
+Start(){
+  # TODO check whether here are the place we need Set-ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned # need privilege
+
+& "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m onPremise -s -c file:.\config.json # need privilege
+}
