77780b01266270f20c8575aaedad355d47df89e0
David Liu <david-khala@hotmail.com>
2024-08-02 08:37:03 +0800

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -74,12 +74,94 @@ function Disable-HyperV {
 function Enable-HyperV {
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 }
-function Add-Path($Path) {
-    $NewPath = [Environment]::GetEnvironmentVariable("PATH", "Machine") + [IO.Path]::PathSeparator + $Path
-    [Environment]::SetEnvironmentVariable( "Path", $NewPath, "Machine" )
-    $env:Path += ";$Path" # make path change take effect immediately
-}
+# function Add-Path($Path) {
+#     $NewPath = [Environment]::GetEnvironmentVariable("PATH", "Machine") + [IO.Path]::PathSeparator + $Path
+#     [Environment]::SetEnvironmentVariable( "Path", $NewPath, "Machine" )
+#     $env:Path += ";$Path" # make path change take effect immediately
+# }
 function Show-Env {
     Get-ChildItem -Path Env:
 
 }
+
+
+# below is fork of https://gist.github.com/mkropat/c1226e0cc2ca941b23a9
+
+function Add-Path {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split [IO.Path]::PathSeparator
+        if ($persistedPaths -notcontains $Path) {
+            $persistedPaths = $persistedPaths + $Path | where { $_ }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join [IO.Path]::PathSeparator, $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split [IO.Path]::PathSeparator
+    if ($envPaths -notcontains $Path) {
+        $envPaths = $envPaths + $Path | where { $_ }
+        $env:Path = $envPaths -join [IO.Path]::PathSeparator
+    }
+}
+
+function Remove-Path {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split [IO.Path]::PathSeparator
+        if ($persistedPaths -contains $Path) {
+            $persistedPaths = $persistedPaths | where { $_ -and $_ -ne $Path }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join [IO.Path]::PathSeparator, $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -contains $Path) {
+        $envPaths = $envPaths | where { $_ -and $_ -ne $Path }
+        $env:Path = $envPaths -join ';'
+    }
+}
+
+function Get-Path {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('Machine', 'User')]
+        [string] $Container
+    )
+
+    $containerMapping = @{
+        Machine = [EnvironmentVariableTarget]::Machine
+        User = [EnvironmentVariableTarget]::User
+    }
+    $containerType = $containerMapping[$Container]
+
+    [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';' |
+        where { $_ }
+}
+
+Export-ModuleMember -Function *
