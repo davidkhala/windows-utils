2a78e2d51d48171cf7da57661056c8481eaa04f5
David Liu <david-khala@hotmail.com>
2025-03-12 20:19:40 +0800

    Update az.ps1

--- azure/az.ps1
+++ azure/az.ps1
@@ -1,3 +1,11 @@
 function Install{
   winget install --exact --id Microsoft.AzureCLI
 }
+function Install-PSModule{
+  # Install the Azure Az PowerShell module
+  Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+
+}
+if ($args.Count -gt 0) {
+    Invoke-Expression ($args -join " ")
+}
