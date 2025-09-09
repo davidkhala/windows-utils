cccda11efb2d642fadf953d614b4b356ac9ecac5
David Liu <david-khala@hotmail.com>
2025-09-10 07:25:26 +0800

    close #8

--- powershell/editor.ps1
+++ powershell/editor.ps1
@@ -1,33 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-function Append {
-  param (
-    [string]$file,
-    [string]$content
-  )
-  Add-Content -Path $file -Value "$content"
-}
-function Configure {
-  param (
-    [string]$file,
-    [string]$key, 
-    [string]$value
-  )
-    
-  
-  $data = Get-Properties $file
-  $data["$key"] = "$value"
-  $data.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" } | Set-Content -Path $file
-}
-function Get-Properties {
-  param (
-    [string]$file
-  )
-  
-  $content = Get-Content -Path $file | Out-String
-  return ConvertFrom-StringData -StringData $content
-
-}
 function Install-JQ {
   winget install jqlang.jq
 }
--- powershell/powershell.ps1
+++ powershell/powershell.ps1
@@ -1,24 +1,4 @@
 $ErrorActionPreference = "Stop"
-function Get-WebFile {
-    # Download file from Uri to local Path
-    param (
-        [string]$Uri,
-        [string]$Path
-    )
-
-    
-    if (!$Path) { 
-        $Path = (Split-Path $Uri -Leaf)
-    }
-    
-    Invoke-WebRequest -Uri $Uri -OutFile $Path -UseBasicParsing
-}
-function Find {
-    param (
-        [string]$Cmd
-    )
-    Get-Command $Cmd
-}
 
 function Use-RemoteSigned {
     # 改为RemoteSigned 执行策略，以允许执行PowerShell脚本
@@ -32,17 +12,7 @@ function Use-SUDO {
 function Use-AnyScript {
     Set-ExecutionPolicy Unrestricted -Force
 }
-function Use-RemoteScript {
-    # source远程脚本，可用于加载远程PowerShell脚本中的所有函数到当前Powershell环境中，以直接调用
-    param (
-        [string]$Uri
-    )
-    (Invoke-WebRequest -Uri $Uri -UseBasicParsing).Content | Invoke-Expression
-}
-function Version {
-    # Get current Powershell version 
-    $PSVersionTable.PSVersion
-}
+
 function Install-Core {
     winget install --id Microsoft.PowerShell --source winget
 }
