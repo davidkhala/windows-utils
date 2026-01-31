e25310e910951f25e3ac50cc4e0b3792f598d834
David Liu <david-khala@hotmail.com>
2026-01-31 09:43:18 +0800

    Update hyper-v.ps1

--- hyper-v.ps1
+++ hyper-v.ps1
@@ -1,6 +1,13 @@
 function Disable {
-    Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V
+    bcdedit /set hypervisorlaunchtype off
 }
 function Enable {
+    bcdedit /set hypervisorlaunchtype auto
+}
+
+function Uninstall{
+    Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V
+}
+function Install {
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 }
