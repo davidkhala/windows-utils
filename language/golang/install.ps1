61d61d1c048637fd4a31fc308d5d2240ed660cc1
David Liu <david-khala@hotmail.com>
2026-07-23 21:30:37 +0800

    Update install.ps1

--- language/golang/install.ps1
+++ language/golang/install.ps1
@@ -3,7 +3,8 @@ function By-Choco{
   choco install go -y
 }
 function By-msi{
-  $installerUrl = "https://go.dev/dl/go1.24.4.windows-amd64.msi"
+  
+  $installerUrl = "https://go.dev/dl/go1.26.5.windows-amd64.msi"
   $installerPath = "go.windows-amd64.msi"
   Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
   Start-Process msiexec.exe -Wait -ArgumentList "/i `"$installerPath`" /quiet"
